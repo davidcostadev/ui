0e57cc9efe114f611d0a45b93285af6153431c72
David Costa <davidcostadev@gmail.com>
2020-10-02 09:55:30 -0300

    build: implement the storybook and deploy this to github

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+      }),
+    ];
+
+    // Return the altered config
+    return config;
+  },
+};
